4e9b3541b7feff00898087ab375c71a5a8e8ab00
Jony-Mia <ajonymia321@gmail.com>
2025-10-08 13:31:25 +0600

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-
+import React from 'react'
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+    <Home/>
+    </>
   )
 }
 
---
+++ src/Pages/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from "./../assets/img/logos/logo-Photoroom (1).png"
+const Header = () => {
+  return (
+    <div>
+
+        <div >
+            <div>
+                <div>
+                    <img width={100} className='' src={Logo} alt="" />
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
